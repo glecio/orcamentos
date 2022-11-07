f4a1250d9593019931b836d22c1dcc8347fac710
Glecio Santos <glecio.santos@gmail.com>
2022-11-07 18:48:44 -0300

    tela de impressao

--- src/pages/Print/index.js
+++ src/pages/Print/index.js
@@ -1,11 +1,35 @@
-import { useLocation } from "react-router-dom"
+    import { useLocation } from "react-router-dom"
 
 
 export default function Print(props){
     const location = useLocation()
-    console.log(location , "state hook")
+    const data = location.state
+    const total = location.total
+    console.log(data , "data hook")
     return (
+        <>
         <h1>Print</h1>
-
+        <table>
+        <thead>
+            <tr>
+                <th>Item</th>
+                <th>quantidade</th>
+                <th>preço</th>
+                <th>subtotal</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.data.map( (peca, index ) => (
+                <tr key={index}>
+                    <td>{peca.nome}</td>
+                    <td>{peca.quantidade}</td>
+                    <td>R${peca.valor}</td>
+                    <td>R${peca.subtotal}</td>
+                </tr>
+            ))}  
+        </tbody>
+    </table>
+    <h1>Total: R${data.total}</h1>
+    </>
     )
 }
