bd6aa07d12112a8a86e53233fd8ffcbd6be58cdd
Glecio Santos <glecio.santos@gmail.com>
2022-11-07 18:30:05 -0300

    rotas da aplicacao

--- src/App.js
+++ src/App.js
@@ -1,71 +1,14 @@
-import {useState} from 'react';
-//import Nome from "./components/Nome";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Print from "./pages/Print";
 
-
-function App(){
-  const [nome,  setNome ] = useState('')
-  const [quantidade, setQuantidade] = useState('')
-  const [valor, setValor] = useState(0)
-
-  const [item,  setItem ] = useState('')
-
-
-
-  
-  function handleRegister(e){
-    e.preventDefault();
-    
-    setItem([
-      ...item,
-      {
-        nome : nome,
-        quantidade : quantidade,
-        valor : valor
-      }] 
-    )
-
-
-    console.log(item)
-
-  }
-
-  
-
-  return (
-    <>
-      <form onSubmit={handleRegister}>
-        <label>Nome:</label>
-        <input placeholder='Digite seu nome'
-          value={nome}
-          onChange={
-            (e) =>  setNome(e.target.value)
-          }
-        /><br/>
-
-        <label>Quantidade:</label>
-        <input placeholder='Digite seu email'
-          value={quantidade}
-          onChange={
-            (e) => setQuantidade(e.target.value)
-          }
-        /><br/>
-
-        <label>Valor:</label>
-        <input placeholder='Digite sua idade'
-          value={valor}
-          onChange={
-            (e) => setValor(e.target.value)
-          }
-        /><br/>
-
-        <button type='submit'>Enviar</button>
-      </form>
-
-      <span>Nome = {item.nome}  </span><br/>
-      <span>Valor = {item.quantidade}</span><br/>
-      <span>Quantidade = {item.valor}</span><br/>
-    </>
+export default function App(){
+  return(
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<Home />} />
+          <Route path="print" element={<Print />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
-
-export default App;
