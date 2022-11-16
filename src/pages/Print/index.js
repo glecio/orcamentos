4c8ee6f913b504c667add982ff7ec425ff4d0945
Glecio Santos <glecio.santos@gmail.com>
2022-11-16 12:54:13 -0300

    logo png

--- src/pages/Print/index.js
+++ src/pages/Print/index.js
@@ -1,5 +1,5 @@
 import { useLocation } from "react-router-dom"
-import Logo from "../../assets/logo.jpeg"
+import Logo from "../../assets/logo.png"
 
 function formataReal (entrada){
     if (typeof(entrada) == 'string'){
